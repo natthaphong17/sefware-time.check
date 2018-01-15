117a4dc7c73f697e990a9b15cb06eec953003acc
ib.natsu17 <r.natthaphong17@gmail.com>
2018-01-15 12:11:59 +0700

    add project sefware-core to project sefware-time.check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,15 +52,6 @@ import { ItemSubGroupComponent } from './setup/item-sub-group/item-sub-group.com
 import { ItemSubGroupDialogComponent } from './setup/item-sub-group/item-sub-group-dialog/item-sub-group-dialog.component';
 import { ItemComponent } from './setup/item/item.component';
 import { ItemDialogComponent } from './setup/item/item-dialog/item-dialog.component';
-import { ImportItemDialogComponent } from './setup/item/import-item-dialog/import-item-dialog.component';
-import { UomComponent } from './setup/uom/uom.component';
-import { UomDialogComponent } from './setup/uom/uom-dialog/uom-dialog.component';
-import { SupplierComponent } from './setup/supplier/supplier.component';
-import { SupplierDialogComponent } from './setup/supplier/supplier-dialog/supplier-dialog.component';
-import { DepartmentComponent } from './setup/department/department.component';
-import { DepartmentDialogComponent } from './setup/department/department-dialog/department-dialog.component';
-import { LocationComponent } from './setup/location/location.component';
-import { LocationDialogComponent } from './setup/location/location-dialog/location-dialog.component';
 
 // Application wide providers
 const APP_PROVIDERS = [
@@ -100,15 +91,6 @@ interface StoreType {
     ItemSubGroupDialogComponent,
     ItemComponent,
     ItemDialogComponent,
-    ImportItemDialogComponent,
-    UomComponent,
-    UomDialogComponent,
-    SupplierComponent,
-    SupplierDialogComponent,
-    DepartmentComponent,
-    DepartmentDialogComponent,
-    LocationComponent,
-    LocationDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -147,15 +129,6 @@ interface StoreType {
     ItemSubGroupDialogComponent,
     ItemComponent,
     ItemDialogComponent,
-    ImportItemDialogComponent,
-    UomComponent,
-    UomDialogComponent,
-    SupplierComponent,
-    SupplierDialogComponent,
-    DepartmentComponent,
-    DepartmentDialogComponent,
-    LocationComponent,
-    LocationDialogComponent,
   ],
   providers: [
     AuthService,
