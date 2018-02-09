2e3b53eb17e39780661cc36ce43dc3b9444e3f7e
ChainarongSG <35440221+ChainarongSG@users.noreply.github.com>
2018-02-09 15:56:56 +0700

    v4.2

--- src/app/main/payrolls/management/management.module.ts
+++ src/app/main/payrolls/management/management.module.ts
@@ -9,6 +9,7 @@ import {BarRatingModule} from 'ngx-bar-rating';
 import {ManagementComponent} from './management.component';
 import {NewPaymentsComponent} from './new-payments/new-payments.component';
 import {PaymentComponent} from './payment/payment.component';
+import { PrintPaymentComponent } from './print-payment/print-payment.component';
 
 @NgModule({
   imports: [
@@ -48,11 +49,13 @@ import {PaymentComponent} from './payment/payment.component';
     ManagementComponent,
     PaymentComponent,
     NewPaymentsComponent,
+    PrintPaymentComponent,
   ],
   entryComponents: [
     ManagementComponent,
     PaymentComponent,
     NewPaymentsComponent,
+    PrintPaymentComponent,
   ],
   providers: [
 
